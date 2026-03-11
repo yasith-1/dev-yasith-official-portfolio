95e6b19ab980079e77ecea0073c81b41c51c39f3
Yasith <141606159+yasith-1@users.noreply.github.com>
2026-03-11 20:25:11 +0530

    feat: add Tawk.to live chat integration to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { cn } from "@/lib/utils";
 import WhatsAppWidget from "@/components/sub/whatsapp-widget";
 import { WHATSAPP_CONFIG } from "@/constants";
 import dynamic from "next/dynamic";
+import Script from "next/script";
 
 import "./globals.css";
 
@@ -41,6 +42,19 @@ export default function RootLayout({ children }: PropsWithChildren) {
           assistantName={WHATSAPP_CONFIG.assistantName}
         /> */}
         <Footer />
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/69b1812315202c1c36a80a79/1jjem0suk';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
